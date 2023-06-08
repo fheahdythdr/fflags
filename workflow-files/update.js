2b7265383b2aae29a8542ceb88a3b2927ad8bb29
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-08 12:56:15 +0200

    no need to filter rn

--- workflow-files/update.js
+++ workflow-files/update.js
@@ -1,34 +1,35 @@
 (async () => {
     const args = process.argv.slice(2);
     const path = require("path")
-    const adder = JSON.parse(args[0]);
+    //const adder = JSON.parse(args[0]);
     const output = '/home/runner/work/rco-fflags/rco-fflags' + args[1];
-    const names = [];
+    /*const names = [];
     const values = [];
-    const includes = [];
+    const includes = [];*/
     const custom = [];
     for (const item1 of adder) {
         if (item1.values.length) {
-            for (const item2 of item1.values) {
+            /*for (const item2 of item1.values) {
                 if (item1.tbl == "names") names.push(item2);
                 else if (item1.tbl == "values") values.push(item2);
                 else if (item1.tbl == "includes") includes.push(item2);
-            }
+            }*/
         }
         else {
             custom.push(item1.values);
         }
     }
     const fs = require('fs')
-    const data = await fetch("https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json")
+    const data = await fetch("https://cdn.discordapp.com/attachments/1101725468815798322/1115958516851675146/ClientAppSettings.json")
     const Settings = await data.json()
     const New = {};
+    /*
     for (const name of Object.keys(Settings)) {
         if (includes.some((val) => name.includes(val)) || names.some((val) => name.startsWith(val)) || values.some((val) => Settings[name] == val)) {
             if (name.includes("TaskSchedulerTargetFps")) Settings[name] = 999999
             New[name] = Settings[name];
         }
-    }
+    }*/
     
     for (const item of custom) {
         for (const name of Object.keys(item)) {
