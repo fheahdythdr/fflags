04f32a1102bf4111dcae6a88c9f793eeb655c4a3
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-09 23:26:22 +0200

    bruh moment 2

--- workflow-files/update.js
+++ workflow-files/update.js
@@ -23,13 +23,13 @@
     const data = await fetch("https://cdn.discordapp.com/attachments/1101725468815798322/1115958516851675146/ClientAppSettings.json")
     const Settings = await data.json()
     const New = {};
-    /*
+    
     for (const name of Object.keys(Settings)) {
-        if (includes.some((val) => name.includes(val)) || names.some((val) => name.startsWith(val)) || values.some((val) => Settings[name] == val)) {
+        //if (includes.some((val) => name.includes(val)) || names.some((val) => name.startsWith(val)) || values.some((val) => Settings[name] == val)) {
             if (name.includes("TaskSchedulerTargetFps")) Settings[name] = 999999
             New[name] = Settings[name];
-        }
-    }*/
+        //}
+    }
     
     for (const item of custom) {
         for (const name of Object.keys(item)) {
